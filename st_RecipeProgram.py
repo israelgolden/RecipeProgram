484aa121e1588f57385b247e39fa20617b88b6a1
Israel Golden <israelagolden@gmail.com>
2023-09-09 00:20:07 -0400

    named file

--- st_RecipeProgram.py
+++ st_RecipeProgram.py
@@ -876,4 +876,7 @@ with col3:
 
 # Display the filtered DataFrame
 st.dataframe(all_recipes_df)
-st.download_button(label = 'Download Recipe Spreadsheet', data = all_recipes_df.to_excel(), mime='text/xlsx')
+st.download_button(label = 'Download Recipe Spreadsheet', 
+                   data = all_recipes_df.to_excel(), 
+                   mime='text/csv', 
+                   file_name='RecipeSpreadsheet')
